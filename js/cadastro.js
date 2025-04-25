3b46af7c254bad229d86e2738c593b8039ad0041
José Fernando <166651961+nand9l@users.noreply.github.com>
2025-04-25 11:16:30 -0300

    Update cadastro.js

--- js/cadastro.js
+++ js/cadastro.js
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const tipo = tipoInput.value;
 
     try {
-      const response = await fetch('http://localhost:5000/api/usuarios', {
+      const response = await fetch('https://odontocrim-api.onrender.com/api/usuarios', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -61,7 +61,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (!tabelaUsuarios || !tbody) return;
 
     try {
-      const res = await fetch('http://localhost:5000/api/usuarios', {
+      const res = await fetch('https://odontocrim-api.onrender.com/api/usuarios', {
         headers: {
           Authorization: `Bearer ${token}`
         }
