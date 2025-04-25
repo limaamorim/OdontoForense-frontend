fe28721c0bf56d59d09983835350d09e63a9727e
José Fernando <166651961+nand9l@users.noreply.github.com>
2025-04-25 11:17:41 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -8,7 +8,7 @@ document.querySelector('form').addEventListener('submit', async function(e) {
   console.log('Senha enviada para login:', senha);
 
   try {
-    const response = await fetch('http://localhost:5000/api/auth/login', {
+    const response = await fetch('https://odontocrim-api.onrender.com/api/auth/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, senha })
