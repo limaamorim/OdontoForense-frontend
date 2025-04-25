9ce4aeed8ff7b835c12f47ad1eac55a39fa3b151
José Fernando <166651961+nand9l@users.noreply.github.com>
2025-04-25 11:16:49 -0300

    Update caso.js

--- js/caso.js
+++ js/caso.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
       };
 
       try {
-        const response = await fetch('http://localhost:5000/api/casos', {
+        const response = await fetch('https://odontocrim-api.onrender.com/api/casos', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
